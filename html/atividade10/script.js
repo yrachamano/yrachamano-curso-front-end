83fbd351d8df4a0eb05512cb4b2af8096dd72ba7
daniel <daniel.andriola@gmail.com>
2023-08-31 11:42:17 -0300

    atualizando

---
+++ html/atividade10/script.js
@@ -0,0 +1,28 @@
+
+//alert('hellow word! com javascript')
+
+//prompt('informe seu nome')
+
+//confirm('tem certeza energumeno?')
+
+//console.log('usando o console')
+    
+// var n1 = "Luciano"
+
+// var n2 = 10
+
+// var soma = n1 / n2
+
+//var caneca = {
+//    nome: 'teu cu',
+//    cor: 'colorida',
+//    educativa: 'true'
+//}
+
+//var exVetor = ['caneca', 'cafe']
+
+
+
+//  console.log(exVetor)
+//
+// console.log(typeof(exVetor))
