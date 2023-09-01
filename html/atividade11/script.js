a8a4d1c7dee1aabf3335747ea49ee32d38b5237b
daniel alves <daniel.andriola@gmail.com>
2023-09-01 11:44:08 -0300

    atualizando

---
+++ html/atividade11/script.js
@@ -0,0 +1,18 @@
+//window.document.getElementsByTagName('h1')
+//console.log(window.document.getElementsByTagName('h1'))
+const titulo = window.document.getElementsByTagName('h1')
+const classe = document.getElementsByClassName('quadrado')
+const li = document.getElementsByTagName('li')
+//querySelector
+//querySelectorAll
+const lista = document.querySelectorAll('ul')
+
+
+
+
+//console.log(li)
+
+//console.log(classe)
+
+console.log(lista)
+//console.log(lista)
