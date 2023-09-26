546b225d37903240464dcd97e53fd8bf8b1a8f6a
Daniel <daniel.andriola@gamail.com>
2023-09-25 22:36:19 -0300

    daqui a pouco eu clono teu cartão

--- html/gpt/script.js
+++ html/gpt/script.js
@@ -25,6 +25,28 @@
 //    console.log(erro)
 //})
 
+const secao = document.querySelector("section")
+
 fetch('https://fakestoreapi.com/products?limit=5')
-            .then(res=>res.json( ))
-            .then(json=>console.log(json))
+            .then(res=>res.json())
+            .then(json=>{
+                console.log(json)
+                let coisas = json
+                criarCoisas(coisas)
+            })
+
+
+function criarCoisas (cositas) {
+    cositas.forEach(element => {
+        let objeto = `<div class="card">
+        <img src="${element.image}">
+        <h4>${element.title}</h4>
+        <span class="description">${element.description}</span>
+        <span class="nota-container">nota : <span class="nota">${element.rating.rate}</span></span>
+        <span class="valor">${element.price}</span> 
+        <button>comprar</button>
+        </div>`
+
+        secao.innerHTML += objeto
+    });
+}
