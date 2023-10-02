d4a349a8ea7c58aa43770567a7daf550d3a7f71f
Daniel <daniel.andriola@gamail.com>
2023-10-02 18:20:45 -0300

    login e cadastro

---
+++ html/barbearia/loguin e cadastro/cadastro.js
@@ -0,0 +1,48 @@
+
+const usernameCadastro = document.querySelector("login")
+const senhaCadastro = document.querySelector("senha")
+const numeroCasa = document.querySelector("#numero_casa")
+const dataNascimento = document.querySelector("#data_nascimento")
+const email = document.querySelector("email")
+
+
+
+
+const btnCadastro = document.querySelector("#enviar_cadastro")
+
+btnCadastro.addEventListener("click", (evento) => {
+    evento.preventDefault()
+    if (logradouro.value == "" || usernameCadastro.value == "" || senhaCadastro.value == "" || numeroCasa.value == "" || dataNascimento.value == "" || email.value == "") {
+        alert("Existem campos há serem completados")
+    } else {
+
+        let logins = []
+        if (localStorage.getItem("logins")) {
+            logins = [...JSON.parse(localStorage.getItem("logins"))]
+        }
+        if ((!logins) || procuraLocal(logins, email.value) == -1) {
+            let login =
+                {
+                    email: email.value,
+                    nome: usernameCadastro.value,
+                    senha: senhaCadastro.value,
+                    dataDeNascimento: dataNascimento.value,
+                    comentarios:''
+                }
+            logins[logins.length] = login
+            localStorage.setItem("logins", JSON.stringify(logins))
+            window.location.href = "./login.html"
+        }
+        else {
+            alert("Email já cadastrado")
+        }
+
+    }
+})
+
+function procuraLocal(elemento, procura) {
+    let resultado = elemento.findIndex(object => {
+        return object.email == procura
+    })
+    return resultado
+}
---
+++ html/barbearia/loguin e cadastro/loguin.js
@@ -0,0 +1,30 @@
+const username = document.getElementById("login")
+const senha = document.getElementById("senha")
+const btnEnviar = document.getElementById("entrar")
+
+let logins = []
+if (localStorage.getItem("logins")) {
+    logins = [...JSON.parse(localStorage.getItem("logins"))]
+}
+
+btnEnviar.addEventListener("click", evento => {
+    evento.preventDefault()
+    console.log(username.value)
+    if(username.value == "" ||  senha.value == "") {
+        alert("Ainda há campos a serem preenchidos")
+    }
+    else {
+        const index = logins.findIndex( object => {
+            return object.email == username.value
+        })
+        if (logins[index].senha == senha.value && index != -1) {
+            localStorage.setItem("isLoggedIn", true)
+            localStorage.setItem("loggedAccount", JSON.stringify(logins[index]))
+            window.location.href = "../index.html"
+        } else if (logins[index].senha != senha.value){
+            alert("Senha incorreta")
+        } else {
+            alert("login não encontrado")
+        }
+    }
+})
